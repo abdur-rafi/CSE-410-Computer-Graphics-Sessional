af54a191d03d416537933a5ec8f95ad01b4b2f7e
abdur-rafi <abdurrafi403@gmail.com>
2023-06-10 11:23:47 +0600

    clock hands with triangular tips

--- clock.cpp
+++ clock.cpp
@@ -4,28 +4,41 @@
 #include <chrono>
 #include <ctime>
 
-
-const float CLOCK_RADIUS = .4f;
-const float LARGE_TICK = .1f;
-const float SMALL_TICK = .05f;
-const float CLOCK_OUTER_RADIUS = .5f;
-const float BOB_RADIUS = .09f;
-const float ROD_LEN = .4f;
-const float JOINT_RADIUS = .05f;
+// clock circles
 const float CLOCK_CENTER_X = 0.0f;
 const float CLOCK_CENTER_Y = 0.3f;
+const float CLOCK_RADIUS = .4f;
+const float CLOCK_OUTER_RADIUS = .5f;
+
+// ticks
+const float LARGE_TICK = .1f;
+const float SMALL_TICK = .05f;
+
+// pendulum
+const float BOB_RADIUS = .09f;
+const float JOINT_RADIUS = .05f;
+const float ROD_LEN = .4f;
+const float ROD_WIDTH = .05f;
 const float ANGLE_DELTA = (M_PI * 2) / 100;
 const float MAX_ANGLE = (M_PI) / 4;
 const float OMEGA = M_PI;
-const float ROD_WIDTH = .05f;
-const int INTERVAL = 40;
-const float HOUR_LEN = .3f;
+
+// hands
+const float HOUR_LEN = .1f;
+const float H_TR_HEIGHT = .1f;
 const float HOUR_WIDTH = .03f;
-const float MIN_LEN = .34f;
+
+const float MIN_LEN = .15f;
+const float M_TR_HEIGHT = .1f;
 const float MIN_WIDTH = .02f;
-const float SEC_LEN = .38;
-const float SEC_WIDTH = .015f;
+
+const float SEC_LEN = .2;
+const float S_TR_HEIGHT = .1f;
+const float SEC_WIDTH = .01f;
+
+// others
 const float EPS = 1e-5;
+const int INTERVAL = 20;
 const float QUARTER = M_PI / 2 ;
 int time_ = 0;
 
@@ -124,7 +137,27 @@ void drawRod(float jointCenterX, float jointCenterY, float bobCenterX, float bob
 
 }
 
-void drawClock(float angle){
+void drawTriag(float mx, float my, float cx, float cy, float width){
+    float angle = getAngleIn2dCoord(mx, my, cx, cy);
+
+    float rodC1x = xFromAngle(angle - QUARTER , width / 2, mx);
+    float rodC1y = yFromAngle(angle - QUARTER , width / 2, my);
+
+
+    float rodC2x = xFromAngle(angle + QUARTER, width / 2, mx);
+    float rodC2y = yFromAngle(angle + QUARTER, width / 2, my);
+
+    glBegin(GL_TRIANGLES);
+        glVertex2f(rodC1x, rodC1y);
+        glVertex2f(rodC2x, rodC2y);
+        glVertex2f(cx, cy);
+
+    glEnd();
+
+
+}
+
+void drawClock(){
     drawCircle(CLOCK_CENTER_X, CLOCK_CENTER_Y, CLOCK_RADIUS, false);
     drawCircle(CLOCK_CENTER_X, CLOCK_CENTER_Y, CLOCK_OUTER_RADIUS, false);
 
@@ -161,11 +194,8 @@ void drawClock(float angle){
 float getTimeAngle(float t, float div){
     return QUARTER - t * (M_PI / div);
 }
-void display(){
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    // pendulum
-
+void drawPendulum(){
     float jointCenterX = CLOCK_CENTER_X;
     float jointCenterY = CLOCK_CENTER_Y - CLOCK_OUTER_RADIUS;
 
@@ -184,8 +214,33 @@ void display(){
     );
     drawRod(jointCenterX, jointCenterY, bobCenterX, bobCenterY, ROD_WIDTH);
 
-    // hands
-    
+}
+
+void drawHand(float angle, float len, float height, float width){
+
+    float mx = xFromAngle(angle ,len, CLOCK_CENTER_X);
+    float my = yFromAngle(angle ,len, CLOCK_CENTER_Y);
+
+    float cx = xFromAngle(angle, len + height, CLOCK_CENTER_X);
+    float cy = yFromAngle(angle, len + height, CLOCK_CENTER_Y);
+
+    drawRod(
+        CLOCK_CENTER_X, 
+        CLOCK_CENTER_Y, 
+        mx,
+        my,
+        width
+    );
+    drawTriag(
+        mx, 
+        my, 
+        cx,
+        cy,
+        width
+    );
+}
+
+void drawHands(){
     std::time_t currentTime = std::time(nullptr);
 
     std::tm* localTime = std::localtime(&currentTime);
@@ -194,39 +249,37 @@ void display(){
     int minute = localTime->tm_min;
     int second = localTime->tm_sec;
 
-    // std::cout << "hour : " << hour << "\n";
 
-    float hourAngle = getTimeAngle(hour + (float)minute/60.0f, 6);
-    // std:: cout << "hour angle " << hourAngle << "\n";
+    float hourAngle = getTimeAngle(hour + (float)minute / 60.0f , 6);
+    
+    // std::cout << "hour " << hour << "hour angle : " << hourAngle << "\n";
+    drawHand(hourAngle, HOUR_LEN, H_TR_HEIGHT, HOUR_WIDTH);
 
-    drawRod(
-        CLOCK_CENTER_X, 
-        CLOCK_CENTER_Y, 
-        xFromAngle(hourAngle ,HOUR_LEN, CLOCK_CENTER_X),
-        yFromAngle(hourAngle ,HOUR_LEN, CLOCK_CENTER_Y),
-        HOUR_WIDTH
-    );
+    // std::cout << mx << " " << my << " " << cx << " " << cy << "\n";
 
     float minuteAngle = getTimeAngle(minute + (float)second / 60.f, 30);
-    drawRod(
-        CLOCK_CENTER_X, 
-        CLOCK_CENTER_Y, 
-        xFromAngle(minuteAngle ,HOUR_LEN, CLOCK_CENTER_X),
-        yFromAngle(minuteAngle ,HOUR_LEN, CLOCK_CENTER_Y),
-        MIN_WIDTH
-    );
+    
+    drawHand(minuteAngle, MIN_LEN, M_TR_HEIGHT, MIN_WIDTH);
+
 
     float secondAngle = getTimeAngle(second, 30);
-    drawRod(
-        CLOCK_CENTER_X, 
-        CLOCK_CENTER_Y, 
-        xFromAngle(secondAngle ,HOUR_LEN, CLOCK_CENTER_X),
-        yFromAngle(secondAngle ,HOUR_LEN, CLOCK_CENTER_Y),
-        MIN_WIDTH
-    );
 
+    drawHand(secondAngle, SEC_LEN, S_TR_HEIGHT, SEC_WIDTH);
+    
 
-    drawClock(angle);
+}
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // pendulum
+    drawPendulum();
+    
+    // hands
+    
+    drawHands();
+
+    drawClock();
 
 
 
