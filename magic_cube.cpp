6050d483edb25fb42f1492e8a5c6d15fb9e6ae4f
abdur-rafi <abdurrafi403@gmail.com>
2023-06-12 07:38:17 +0600

    main func maybe done

--- magic_cube.cpp
+++ magic_cube.cpp
@@ -1,6 +1,7 @@
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 /* Initialize OpenGL Graphics */
 void initGL() {
@@ -234,20 +235,105 @@ void drawSphere(double radius, int stacks, int slices) {
     glEnd();
 }
 
-void drawSphereAtCorner(int x, int y, int z){
+
+std::vector<std::vector<point>> buildUnitPositiveX(int subdivision)
+{
+    const float DEG2RAD = acos(-1) / 180.0f;
+
+    std::vector<float> vertices;
+    std::vector<std::vector<point>> vs;
+    float n1[3];        // normal of longitudinal plane rotating along Y-axis
+    float n2[3];        // normal of latitudinal plane rotating along Z-axis
+    float v[3];         // direction vector intersecting 2 planes, n1 x n2
+    float a1;           // longitudinal angle along Y-axis
+    float a2;           // latitudinal angle along Z-axis
+
+    // compute the number of vertices per row, 2^n + 1
+    int pointsPerRow = (int)pow(2, subdivision) + 1;
+
+    // rotate latitudinal plane from 45 to -45 degrees along Z-axis (top-to-bottom)
+    for(unsigned int i = 0; i < pointsPerRow; ++i)
+    {
+        // normal for latitudinal plane
+        // if latitude angle is 0, then normal vector of latitude plane is n2=(0,1,0)
+        // therefore, it is rotating (0,1,0) vector by latitude angle a2
+        a2 = DEG2RAD * (45.0f - 90.0f * i / (pointsPerRow - 1));
+        n2[0] = -sin(a2);
+        n2[1] = cos(a2);
+        n2[2] = 0;
+        
+        std::vector<point> curr;
+
+        // rotate longitudinal plane from -45 to 45 along Y-axis (left-to-right)
+        for(unsigned int j = 0; j < pointsPerRow; ++j)
+        {
+            // normal for longitudinal plane
+            // if longitude angle is 0, then normal vector of longitude is n1=(0,0,-1)
+            // therefore, it is rotating (0,0,-1) vector by longitude angle a1
+            a1 = DEG2RAD * (-45.0f + 90.0f * j / (pointsPerRow - 1));
+            n1[0] = -sin(a1);
+            n1[1] = 0;
+            n1[2] = -cos(a1);
+
+            // find direction vector of intersected line, n1 x n2
+            v[0] = n1[1] * n2[2] - n1[2] * n2[1];
+            v[1] = n1[2] * n2[0] - n1[0] * n2[2];
+            v[2] = n1[0] * n2[1] - n1[1] * n2[0];
+
+            // normalize direction vector
+            float scale = 1 / sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+            v[0] *= scale;
+            v[1] *= scale;
+            v[2] *= scale;
+
+            // add a vertex into array
+            vertices.push_back(v[0]);
+            vertices.push_back(v[1]);
+            vertices.push_back(v[2]);
+            curr.push_back({v[0], v[1], v[2]});
+        }
+        vs.push_back(curr);
+    }
+
+    return vs;
+}
+
+auto x = buildUnitPositiveX(5);
+
+void drawSphereSide(){
     glPushMatrix();
-        glTranslatef(scale * x,scale * y, scale * z);
-        drawSphere((1-scale) * sphereMaxR, 100, 100);
+        glColor3f(.5, 1, .5);
+        glTranslatef(scale, 0, 0);
+        glScalef((1-scale) / sqrt(3) , (1-scale) / sqrt(3), (1-scale) / sqrt(3));
+        glBegin(GL_QUADS);
+        int n = x.size();
+        for(int i = 0; i + 1 < n; ++i){
+            int m = x[i].size();
+            for(int j = 0; j + 1 < m; ++j){
+                glVertex3f(x[i][j].x, x[i][j].y,x[i][j].z);
+                glVertex3f(x[i+1][j].x, x[i+1][j].y,x[i+1][j].z);
+                glVertex3f(x[i+1][j+1].x, x[i+1][j+1].y,x[i+1][j+1].z);
+                glVertex3f(x[i][j+1].x, x[i][j+1].y,x[i][j+1].z);
+            }
+        }
+        glEnd();
     glPopMatrix();
 }
-void drawSpheres(){
-    drawSphereAtCorner(1, 0, 0);
-    drawSphereAtCorner(0, 1, 0);
-    drawSphereAtCorner(0, 0, 1);
 
-    drawSphereAtCorner(-1, 0, 0);
-    drawSphereAtCorner(0, -1, 0);
-    drawSphereAtCorner(0, 0, -1);
+void drawSpheres(){
+    glPushMatrix();
+        drawSphereSide();
+        glRotatef(90, 0, 1, 0);
+        drawSphereSide();
+        glRotatef(90, 0, 1, 0);
+        drawSphereSide();
+        glRotatef(90, 0, 1, 0);
+        drawSphereSide();
+        glRotatef(90, 0, 0, 1);
+        drawSphereSide();
+        glRotatef(-90, 0, 0, 1);
+        drawSphereSide();
+    glPopMatrix();
 }
 
 void display() {
@@ -284,9 +370,16 @@ void display() {
     // drawSide(0, 0, 1, 0, 0);
     // drawSide(90, 0, 1, 0, 1);
     drawSides();
-
     drawSpheres();
-
+    // drawSpheres();
+    
+    // glPushMatrix();
+    //     glRotatef(90, 0, 1, 0);
+    //     drawSphereSide();
+    //     glRotatef(90, 0, 1, 0);
+    //     drawSphereSide();
+    // glPopMatrix();
+    
     // drawSphere(.3, 500, 500);
     // double r = (1 - scale) * cylinderMaxR;
     // double off = ((1 - scale) * cylinderMaxDist) / sqrt(2);
@@ -418,6 +511,13 @@ int main(int argc, char** argv) {
     glutReshapeFunc(reshapeListener);           // Register callback handler for window re-shape
     glutKeyboardFunc(keyboard);
     glutSpecialFunc(special);
+    auto x = buildUnitPositiveX(3);
+    std::cout << x[0].size() << "\n";
+    // std::cout << x.size() << "\n";
+    // for(int i = 0; i < x.size()/3; ++i){
+    //     int b = 3 * i;
+    //     std::cout << x[b] << " " << x[b+1] << " " << x[b + 2] << "\n";
+    // }
     initGL();                                   // Our own OpenGL initialization
     glutMainLoop();                             // Enter the event-processing loop
     return 0;
