63c2ee335e23682a91b12e71cd738a8be0854882
abdur-rafi <abdurrafi403@gmail.com>
2023-11-25 15:00:04 +0600

    read me

--- 1805008/1805008_config.cpp
+++
@@ -1,9 +0,0 @@
-#include "1805008_config.hpp"
-#include <fstream>
-
-void Config::parseConfig(std::ifstream &f){
-
-    f >> near >> far >> fovY >> ratio;
-    f >> levelOfRecursion >> w;
-    
-}
--- run.sh
+++
@@ -1,3 +0,0 @@
-rm app
-g++ 1805008_main.cpp  -o app -lglut -lGLU -lGL 1805008_objects.cpp 1805008_utility.cpp 1805008_config.cpp 1805008_raytracer.cpp 1805008_lightSource.cpp
-./app
